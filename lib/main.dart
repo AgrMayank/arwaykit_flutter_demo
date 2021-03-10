386a75d9562784a665856b8121ab4e27885fd85e
Mayank Raj <myid.mayank@gmail.com>
2021-03-10 20:32:00 +0530

    Integration with ARWAY SDK 0.0.9b

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+import 'menu_screen.dart';
+import 'arwaysdk_unity_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: UnityDemoScreen()
-  ));
+  runApp(MyApp());
 }
 
-class UnityDemoScreen extends StatefulWidget {
-
-  UnityDemoScreen({Key key}) : super(key: key);
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  _UnityDemoScreenState createState() => _UnityDemoScreenState();
-}
-
-class _UnityDemoScreenState extends State<UnityDemoScreen>{
-  static final GlobalKey<ScaffoldState> _scaffoldKey =
-  GlobalKey<ScaffoldState>();
-  UnityWidgetController _unityWidgetController;
-
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      key: _scaffoldKey,
-      body: SafeArea(
-        bottom: false,
-        child: WillPopScope(
-          onWillPop: () {
-            // Pop the category page if Android back button is pressed.
-          },
-          child: Container(
-            color: Colors.yellowAccent,
-            child: UnityWidget(
-              onUnityCreated: onUnityCreated,
-            ),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      title: 'My Flutter App',
+      theme: ThemeData.dark(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MenuScreen(),
+        '/unity': (context) => ARwayKitUnityScreen(),
+      },
     );
-  }
-
-  // Callback that connects the created controller to the unity controller
-  void onUnityCreated(controller) {
-    this._unityWidgetController = controller;
   }
 }
